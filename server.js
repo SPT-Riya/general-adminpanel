1e3f27163d89ab87e5b617c2c8c3f4cabb605479
Meer <meer.spt@gmail.com>
2021-08-31 18:48:34 +0530

    frst

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require("path");
 const dotenv = require("dotenv");
 const app = express();
 const route = require("./server/router/route");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-dotenv.config({ path: ".env" });
+console.log(port);
+dotenv.config({ path: "config.env" });
 connectDB();
 app.use(express.urlencoded({ extended: true }));
 
