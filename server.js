3d3ffe72897bdfa4bfcec24eaeaee3e86387ffe2
Meer <meer.spt@gmail.com>
2021-09-01 15:25:16 +0530

    forth

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use((req, res, next) => {
     "Access",
     "Authorization"
   );
-  // if (require.method === "OPTIONS") {
-  //   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  //   return res.status(200).json({});
-  // }
+  if (require.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    return res.status(200).json({});
+  }
   next();
 });
 
